8261f4c11b41b379a04c950ceff3f2f5ffb3496a
isakpulkki <isak@pulkki.me>
2024-12-03 17:19:23 +0200

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,66 @@
-import './App.css';
+import { createTheme } from '@mui/material/styles';
+import {
+  BrowserRouter as Router, Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import MainPage from './pages/Main';
+import { ThemeProvider } from '@mui/material/styles';
+import NewListingPage from './pages/NewListing';
+import MessagesPage from './pages/Messages';
+import LoginPage from './pages/Login';
+import Layout from './layouts/Layout';
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const theme = createTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: { main: '#3B3B3B' },
+        secondary: { main: '#3B3B3B' },
+      },
+    },
+  },
+  typography: {
+    allVariants: {
+      color: 'black',
+    },
+    fontFamily: 'DM Sans, sans-serif',
+    button: {
+      textTransform: 'none',
+      fontFamily: 'DM Sans, sans-serif',
+      letterSpacing: '.1rem',
+    },
+  },
+});
 
 function App() {
-
   return (
-    <>
-      <div>
-        The app will be here.
-      </div>
-    </>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="newlisting" element={<NewListingPage />} />
+            <Route path="messages" element={<MessagesPage />} />
+            <Route path="login" element={<LoginPage />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
