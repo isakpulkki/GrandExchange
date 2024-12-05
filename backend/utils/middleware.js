af1b23e653b3a8d53df770bc28c459814bfdf8a6
isakpulkki <isak@pulkki.me>
2024-12-06 00:33:00 +0200

    Middleware for the login token

---
+++ backend/utils/middleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const tokenExtractor = (request, response, next) => {
+  const token = request.get('authorization')?.replace('Bearer ', '');
+  if (token) {
+    request.token = token;
+  }
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  const { token } = request;
+
+  if (!token) {
+    return response.status(401).json({ error: 'Token is missing.' });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    request.user = await User.findById(decodedToken.id);
+    if (!request.user) {
+      return response.status(401).json({ error: 'Token is invalid.' });
+    }
+    next();
+  } catch (error) {
+    response.status(401).json({ error: 'Token verification failed.' });
+  }
+};
+
+module.exports = { tokenExtractor, userExtractor };
