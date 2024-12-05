6a435321e4d0b96fdcffdd0b8492051036c1e3f3
isakpulkki <isak@pulkki.me>
2024-12-06 00:31:30 +0200

    Add a placeholder text

--- frontend/src/pages/Messages.tsx
+++ frontend/src/pages/Messages.tsx
@@ -1,3 +1,18 @@
+import { Box, Typography } from '@mui/material';
+
 export default function Messages() {
-  return <></>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        This feature is coming soon!
+      </Typography>
+    </Box>
+  );
 }
