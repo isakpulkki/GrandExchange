068fbca23a01451c5aa6023ec5eea0be840e4f16
isakpulkki <isak@pulkki.me>
2024-12-06 00:33:44 +0200

    Simple Logout page which erases the token

---
+++ frontend/src/pages/Logout.tsx
@@ -0,0 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+export default function Logout() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    localStorage.removeItem('token');
+    navigate('/');
+  }, [navigate]);
+
+  return null;
+}
