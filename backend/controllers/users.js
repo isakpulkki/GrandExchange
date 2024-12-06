5972ad8737c5ac25d8ac73909e6dff0dfab8cc73
isakpulkki <isak@pulkki.me>
2024-12-06 03:37:07 +0200

    Not Exist page

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,10 +4,10 @@ const usersRouter = require('express').Router();
 const User = require('../models/user');
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('blogs', {
-    url: 1,
+  const users = await User.find({}).populate('listings', {
     title: 1,
-    author: 1,
+    description: 1,
+    price: 1,
   });
   response.json(users);
 });
