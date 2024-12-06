763f10772860e8a8148cd0bbc0434ab80fcec2a5
isakpulkki <isak@pulkki.me>
2024-12-06 03:24:50 +0200

    Redirect to main page for now

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const listingsRouter = require('./controllers/listings');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
+const path = require('path');
 
 const app = express();
 
@@ -16,11 +17,14 @@ mongoose
   .catch(() => console.log('Error connecting to MongoDB.'));
 
 app.use(cors());
-app.use(express.static('../frontend/dist'));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.use(express.json());
 app.use(middleware.tokenExtractor);
 app.use('/api/listings', listingsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+});
 
 module.exports = app;
