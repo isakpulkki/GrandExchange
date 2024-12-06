89012d1d8f421690e9a32ea990b8785b2a2fe285
isakpulkki <isak@pulkki.me>
2024-12-06 03:10:40 +0200

    Fix React.js routes to work with Express

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,9 @@ mongoose
 
 app.use(cors());
 app.use(express.static('../frontend/dist'));
+app.get('*', function(req, res) {
+  res.sendFile('../frontend/dist');
+});
 app.use(express.json());
 app.use(middleware.tokenExtractor);
 app.use('/api/listings', listingsRouter);
