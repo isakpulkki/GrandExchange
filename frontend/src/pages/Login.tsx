3c4173a0ab9718bdf4e3db7c6b5dde55e46be347
isakpulkki <isak@pulkki.me>
2024-12-17 20:57:57 +0200

    Refactor logic

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,23 +1,5 @@
-import { Typography, Link } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 export default function Login() {
-  const navigate = useNavigate();
-
-  return (
-    <>
-      <AuthForm type="login" />
-      <Typography align="center" sx={{ mt: 2 }}>
-        Don’t have an account?{' '}
-        <Link
-          component="button"
-          onClick={() => navigate('/register')}
-          sx={{ cursor: 'pointer' }}
-        >
-          Register here
-        </Link>
-      </Typography>
-    </>
-  );
+  return <AuthForm type="login" />;
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,23 +1,5 @@
-import { Typography, Link } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 export default function Register() {
-  const navigate = useNavigate();
-
-  return (
-    <>
-      <AuthForm type="register" />
-      <Typography align="center" sx={{ mt: 2 }}>
-        Already have an account?{' '}
-        <Link
-          component="button"
-          onClick={() => navigate('/login')}
-          sx={{ cursor: 'pointer' }}
-        >
-          Log in here
-        </Link>
-      </Typography>
-    </>
-  );
+  return <AuthForm type="register" />;
 }
