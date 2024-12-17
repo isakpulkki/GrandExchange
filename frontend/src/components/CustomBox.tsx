2fa5c5e8223da90ba0e36d85f323258f9ae534f4
isakpulkki <isak@pulkki.me>
2024-12-17 20:58:22 +0200

    Refactor

--- frontend/src/components/CustomBox.tsx
+++ frontend/src/components/CustomBox.tsx
@@ -3,21 +3,24 @@ import React from 'react';
 import { Box, SxProps, Theme } from '@mui/material';
 
 interface CustomBoxProps {
-  children: React.ReactNode; // This allows any valid React children (string, JSX, etc.)
-  sx?: SxProps<Theme>; // This type ensures sx is compatible with MUI theme styles
+  children: React.ReactNode;
+  sx?: SxProps<Theme>;
 }
 
 const CustomBox: React.FC<CustomBoxProps> = ({ children, sx, ...props }) => {
   return (
     <Box
       sx={{
-        maxWidth: 600,
+        maxWidth: 800,
         margin: '0 auto',
         padding: 2,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        ...sx, // Apply any additional styles passed through the sx prop
+        ...sx,
+        justifyContent: 'center',
+        height: '80vh',
+        textAlign: 'center',
       }}
       {...props}
     >
