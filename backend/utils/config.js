5c411cbc4a2549245581555b21380d07dfb1f836
isakpulkki <isak@pulkki.me>
2024-12-17 18:58:56 +0200

    Limits for listings

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -6,7 +6,26 @@ const MONGODB_URI =
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 
+const USERNAME_LIMITS = {
+  MIN_LENGTH: 3, 
+  MAX_LENGTH: 20, 
+};
+
+const PASSWORD_LIMITS = {
+  MIN_LENGTH: 8,  
+  MAX_LENGTH: 64, 
+};
+
+const LISTING_LIMITS = {
+  TITLE_MAX_LENGTH: 80, 
+  DESCRIPTION_MAX_LENGTH: 500, 
+  PRICE_MAX_LENGTH: 10,  
+};
+
 module.exports = {
   SECRET,
-  MONGODB_URI
+  MONGODB_URI,
+  USERNAME_LIMITS,
+  PASSWORD_LIMITS,
+  LISTING_LIMITS,
 };
