3165e662bcd33b89a2fb24c18cc876cb17c901b7
isakpulkki <isak@pulkki.me>
2025-01-10 03:07:41 +0200

    Route to GET images

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const listingsRouter = require('./controllers/listings');
 const usersRouter = require('./controllers/users');
 const categoriesRouter = require('./controllers/categories');
 const loginRouter = require('./controllers/login');
+const { uploadsRouter } = require('./controllers/uploads');
 const middleware = require('./utils/middleware');
 const config = require('./utils/config');
 const path = require('path');
@@ -25,6 +26,7 @@ app.use(middleware.tokenExtractor);
 app.use('/api/listings', listingsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+app.use('/api/uploads', uploadsRouter);
 app.use('/api/categories', categoriesRouter);
 app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
