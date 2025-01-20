d44222c5c7fd3f5e947be093bb8e5b37f94e13a5
isakpulkki <isak@pulkki.me>
2025-01-20 23:10:07 +0200

    Add a adminExtractor middleware

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -13,7 +13,6 @@ const tokenExtractor = (request, response, next) => {
 
 const userExtractor = async (request, response, next) => {
   const { token } = request;
-
   if (!token) {
     return response.status(401).json({ error: 'Token is missing.' });
   }
@@ -35,4 +34,25 @@ const userExtractor = async (request, response, next) => {
   }
 };
 
-module.exports = { tokenExtractor, userExtractor };
+const adminExtractor = async (request, response, next) => {
+  const { token } = request;
+  if (!token) {
+    return next();
+  }
+  try {
+    const decodedToken = jwt.verify(token, config.SECRET);
+    const user = await User.findById(decodedToken.id);
+
+    if (!user) {
+      return next();
+    }
+    if (user.admin) {
+      request.isAdmin = true;
+    }
+    next();
+  } catch (error) {
+    return next();
+  }
+};
+
+module.exports = { tokenExtractor, userExtractor, adminExtractor };
